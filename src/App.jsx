f1f28bdabf474991982d63a80c044476a5cb27f1
SirioRedGiant <Oiris1992@gmail.com>
2026-02-16 15:29:33 +0100

    Choosed the proper checkbox to use in my project

--- src/App.jsx
+++ src/App.jsx
@@ -53,4 +53,18 @@ export default function App() {
         console.error("Errore durante l'invio:", err);
       });
   };
+   return (
+    <div class="form-check">
+  <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+  <label class="form-check-label" for="flexCheckDefault">
+    Default checkbox
+  </label>
+</div>
+<div class="form-check">
+  <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
+  <label class="form-check-label" for="flexCheckChecked">
+    Checked checkbox
+  </label>
+</div>
+   )
 }
