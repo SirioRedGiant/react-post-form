3abcdb001989ac11e15d3610c2550437f21e8e9d
SirioRedGiant <Oiris1992@gmail.com>
2026-02-16 15:21:11 +0100

    refactor App component: move initialForm definition outside of the component and improve input handling logic --> gestita la chiamata post e la cattura degli imput.

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,54 @@ import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useState } from "react";
 
+//! REACT DOCS --> updating Objects in State
+//note --> creare un oggetto(stampino) che contiene tutti i campi del form. Questo oggetto verrà inviato al server.
+const initialForm = {
+  author: "",
+  title: "",
+  body: "",
+  public: false, // per gestire la checkbox --> parte come bozza
+};
+
 export default function App() {
-  //note --> creare un oggetto che contiene tutti i campi del form. Questo oggetto verrà inviato al server.
-  const initialForm = {
-    author: "",
-    title: "",
-    body: "",
-    public: false, // per gestire la checkbox --> parte come bozza
+  // lo stato che contiene i dati del form mentre l'utente scrive
+  const [formData, detFormData] = useState(initialForm);
+
+  //note --> FUNZIONE UNIVERSALE DEGLI INPUT
+  const handleInputChange = (e) => {
+    //*   e.target è l'elemento HTML che ha scatenato l'evento (l'input)
+    //*   Usiamo il "destructuring" per estrarre le proprietà che ci servono
+    const { name, value, type, checked } = e.target;
+
+    //note --> Aggiora lo stato in modo dinamico
+    setFormData({
+      ...formData, //  Copio l'oggetto attuale per non perdere gli altri campi
+      //   [name] per identificare quale chiave dell' array aggiornare
+      //    Se l'input è una checkbox usiamo "checked", altrimenti "value"
+      [name]: type === "checkbox" ? checked : value,
+    });
   };
 
-  const [formData, detFormData] = useState(initialForm);
+  //note --> FUNZIONE INVIO (POST)
+  const handleSubmit = (e) => {
+    e.preventDefault(); // Previene il refresh della pagina
+
+    const url = "https://**67c5b4f3351c081993fb1ab6**.mockapi.io**/api**/posts";
+
+    // Invia i dati al server (POST)
+    // Passa l'oggetto "formData" scritto nel form
+    axios
+      .post(url, formData)
+      .then((res) => {
+        // Se il server risponde stampa il risultato in console
+        console.log("Dati inviati con successo:", res.data);
+        alert("Post creato!");
+
+        // riporta il form allo stato iniziale --> se ha successo
+        setFormData(initialForm);
+      })
+      .catch((err) => {
+        console.error("Errore durante l'invio:", err);
+      });
+  };
 }
