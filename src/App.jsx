633dd4549e69b5c804544cbf995b820789dc49e7
SirioRedGiant <Oiris1992@gmail.com>
2026-02-16 14:25:35 +0100

    add axios dependency and initialize form state in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,16 @@
 import "./assets/css/index.css";
+import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
-export default function App() {}
+import { useState } from "react";
+
+export default function App() {
+  //note --> creare un oggetto che contiene tutti i campi del form. Questo oggetto verrà inviato al server.
+  const initialForm = {
+    author: "",
+    title: "",
+    body: "",
+    public: false, // per gestire la checkbox --> parte come bozza
+  };
+
+  const [formData, detFormData] = useState(initialForm);
+}
